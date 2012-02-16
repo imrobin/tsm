24f0ddd79f1f5a84b36d168f6ba48c7034311cc7
haojinghua <haojinghua@477ec3fe-02ac-314e-bbf5-a460934ddf39>
2012-02-16 09:27:03 +0000

    格式化代码
    git-svn-id: http://118.122.114.104:8881/svn/tsm/trunk@438 477ec3fe-02ac-314e-bbf5-a460934ddf39

--- tsm/src/main/java/com/justinmobile/tsm/endpoint/manager/impl/PushSmsManagerImpl.java
+++ tsm/src/main/java/com/justinmobile/tsm/endpoint/manager/impl/PushSmsManagerImpl.java
@@ -8,8 +8,8 @@ import com.justinmobile.core.dao.OracleSequenceDao;
 import com.justinmobile.core.exception.PlatformErrorCode;
 import com.justinmobile.core.exception.PlatformException;
 import com.justinmobile.core.manager.EntityManagerImpl;
-import com.justinmobile.core.utils.ConvertUtils;
-import com.justinmobile.core.utils.HexUtils;
+import com.justinmobile.core.utils.ByteUtils;
+import com.justinmobile.core.utils.LvObject;
 import com.justinmobile.tsm.application.dao.ApplicationClientInfoDao;
 import com.justinmobile.tsm.application.dao.ApplicationVersionDao;
 import com.justinmobile.tsm.application.domain.ApplicationClientInfo;
@@ -27,26 +27,27 @@ import com.justinmobile.tsm.transaction.domain.LocalTransaction.Operation;
 import com.justinmobile.tsm.utils.SystemConfigUtils;
 
 @Service("pushSmsManager")
-public class PushSmsManagerImpl extends EntityManagerImpl<PushSms, PushSmsDao> implements PushSmsManager {
+public class PushSmsManagerImpl extends EntityManagerImpl<PushSms, PushSmsDao>
+		implements PushSmsManager {
 
 	@Autowired
 	private PushSmsDao pushSmsDao;
-	
+
 	@Autowired
 	private CustomerCardInfoDao cciDao;
-	
+
 	@Autowired
 	private CardInfoDao cardInfoDao;
-	
+
 	@Autowired
 	private ApplicationClientInfoDao aciDao;
-	
+
 	@Autowired
 	private ApplicationVersionDao appVerDao;
-	
+
 	@Autowired
 	private OracleSequenceDao oracleSequenceDao;
-	
+
 	@Autowired
 	private SmsEndpoint smsEndpoint;
 
@@ -64,41 +65,59 @@ public class PushSmsManagerImpl extends EntityManagerImpl<PushSms, PushSmsDao> i
 	}
 
 	@Override
-	public void sendPushSms(String cardNo, String aid,String version,
+	public void sendPushSms(String cardNo, String aid, String version,
 			Operation operation) {
-		 try{
-			 CardInfo card = cardInfoDao.getByCardNo(cardNo);
-			 CustomerCardInfo cci = cciDao.getByCard(card);
-			 ApplicationVersion appVer = appVerDao.getAidAndVersionNo(aid, version);
-			 PushSms ps = new PushSms();
-			 ps.setAid(aid);
-			 ps.setCardNo(cardNo);
-			 ps.setVersion(version);
-			 ps.setMobileNo(cci.getMobileNo());
-			 ps.setOperate(operation);
-			 String srcPort = SystemConfigUtils.getPushSrcPort();
-			 String destPort = SystemConfigUtils.getPushDestPort();
-			 String serial = oracleSequenceDao.getNextSerialNo("PUSHSERIAL", 12);
-			 //根据应用版本获取 clientId
-			 String osVersion = cci.getMobileType().getOriginalOsKey();
-			 ApplicationClientInfo aci = aciDao.getByApplicationVersionTypeVersionFileType(appVer, ApplicationClientInfo.SYS_TYPE_Android, osVersion, ApplicationClientInfo.FILE_TYPE_APK);
-			 String clientId = "";
-			 if(aci!=null){
-			   clientId = String.valueOf(aci.getId());
-			   ConvertUtils.long2HexString(aci.getId(), 8);
-			 }
-			 ps.setSerial(serial);
-			 pushSmsDao.saveOrUpdate(ps);
-			 smsEndpoint.pushMessage(cci.getMobileNo(), MessageFormat.MSG_FORMAT_TYPE_GBK.getValue(), destPort, srcPort, clientId, cci.getCard().getCardNo(), serial);
-			 System.out.println("success");
-		 }catch (PlatformException e) {
-				throw e;
-			} catch (HibernateException e) {
-				throw new PlatformException(PlatformErrorCode.DB_ERROR, e);
-			} catch (Exception e) {
-				throw new PlatformException(PlatformErrorCode.UNKNOWN_ERROR, e);
+		try {
+			CardInfo card = cardInfoDao.getByCardNo(cardNo);
+			CustomerCardInfo cci = cciDao.getByCard(card);
+			ApplicationVersion appVer = appVerDao.getAidAndVersionNo(aid,
+					version);
+			PushSms ps = new PushSms();
+			ps.setAid(aid);
+			ps.setCardNo(cardNo);
+			ps.setVersion(version);
+			ps.setMobileNo(cci.getMobileNo());
+			ps.setOperate(operation);
+			String srcPort = SystemConfigUtils.getPushSrcPort();
+			String destPort = SystemConfigUtils.getPushDestPort();
+			String serial = oracleSequenceDao.getNextSerialNo("PUSHSERIAL", 12);
+			pushSmsDao.saveOrUpdate(ps);
+			// 根据应用版本获取 clientId
+			String osVersion = cci.getMobileType().getOriginalOsKey();
+			ApplicationClientInfo aci = aciDao
+					.getByApplicationVersionTypeVersionFileType(appVer,
+							ApplicationClientInfo.SYS_TYPE_Android, osVersion,
+							ApplicationClientInfo.FILE_TYPE_APK);
+			String clientId = "";
+			// 将客户端ID和CardNo组成Lv对象
+			if (aci != null) {
+				clientId = String.valueOf(aci.getId());
+				LvObject lvClientId = new LvObject();
+				lvClientId.add(ByteUtils.toHexString(clientId.getBytes()));
+				clientId = lvClientId.build();
 			}
-		
+			// CardNo组装Lv
+			LvObject lvCardNo = new LvObject();
+			lvCardNo.add(ByteUtils.toHexString(cardNo.getBytes()));
+			cardNo = lvCardNo.build();
+			// Serial组装Lv
+			LvObject lvSerial = new LvObject();
+			lvSerial.add(ByteUtils.toHexString(serial.getBytes()));
+			serial = lvSerial.build();
+			destPort = ByteUtils.toHexString(destPort.getBytes());
+			srcPort = ByteUtils.toHexString(srcPort.getBytes());
+			smsEndpoint.pushMessage(cci.getMobileNo(),
+					MessageFormat.MSG_FORMAT_TYPE_GBK.getValue(), destPort,
+					srcPort, clientId, cci.getCard().getCardNo(), serial);
+			System.out.println("success");
+		} catch (PlatformException e) {
+			throw e;
+		} catch (HibernateException e) {
+			throw new PlatformException(PlatformErrorCode.DB_ERROR, e);
+		} catch (Exception e) {
+			throw new PlatformException(PlatformErrorCode.UNKNOWN_ERROR, e);
+		}
+
 	}
 
 }
